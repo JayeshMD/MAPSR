82a81b24bb4e763868b1f2152ba604d6bd8454f7
Jayesh Dhadphale <jayesh@Jayeshs-MacBook-Pro.local>
2022-05-03 01:27:35 +0530

    Implemented merge() and merge_vw() functions in schemes.py to in merge delays and remove coressponding rows and columns from W array.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from cmath import tau
 import torch
 import matplotlib.pyplot as plt
-from schemes import rk4, interp_linear
+import schemes as sc
+
 
 # Data type and device setup
 dtype = torch.float
@@ -14,37 +16,25 @@ x_true = torch.sin(2 * torch.pi * freq * t_true)
 fig = plt.figure()
 axs = fig.add_subplot(1, 1, 1)
 axs.plot(t_true, x_true)
-
 plt.show()
+
 
 dt = t_true[1] - t_true[0]
 Nc = 100
 
 
-##
-
-
-
-##
-def model_torch(t, xx, w0):
+def model(t, xx, w0):
     return torch.mm(xx, w0)
 
 
 ##
-τ = torch.tensor([0.2], device=device, dtype=dtype, requires_grad=True)
-# w0 = torch.randn((2,2), device=device, dtype=dtype, requires_grad=True)
-w0 = torch.zeros((2, 2), device=device, dtype=dtype, requires_grad=True)
-# a = torch.randn((2,2), dtype=dtype).abs()*1e-5
-# b = torch.normal(mean=a*0., std=a)
-# w0 = b.clone().detach().requires_grad_(True).to(device)
-
-# w = [[0,1],[-(2*torch.pi)**2, 0]]
-# w0 = torch.tensor(w, device=device, dtype=dtype, requires_grad=True)
-# w0 = torch.tensor([[0, 6.28],[-6.28,0]], device=device, dtype=dtype, requires_grad=True)
+#τ = torch.linspace(0,0.3, 3, device=device, dtype=dtype, requires_grad=True)
+τ = torch.tensor([0,0.3], device=device, dtype=dtype, requires_grad=True)
+w0 = torch.zeros((len(τ), len(τ)), device=device, dtype=dtype, requires_grad=True)
 
 ##
 lr_tau = 1e-4
-lr_w0 = 1
+lr_w0 = 1e0
 batch_size = 20
 batch_time = 10  # (x_true)-Nc
 lr_pow_w0 = torch.linspace(0, -2, 100)
@@ -53,21 +43,22 @@ lr_pow_tau = torch.linspace(0, -4, 100)
 loss_arr = []
 τ_arr = []
 τ_grad_arr = []
+acc = (t_true[1]-t_true[0])*0.5
 
 ##
 fig = plt.figure()
-
-for kk in range(2000):
+axs = fig.add_subplot(1, 1, 1)
+for kk in range(3000):
 
     # lr_w0 = 1e-4 + 1e-1 * torch.tanh(torch.tensor(kk/500))
     # lr_tau = 1e-1 * torch.tensor(10).pow(lr_pow_tau[kk//100])
-    batch_time = 10 + ((kk // 100) * 10);
+    batch_time = 10 + ((kk // 100) * 10)
 
     if kk % 100 == 0:
         print('lr_w0=', lr_w0)
         print('batch_time=', batch_time)
 
-    z_true = interp_linear(t_true,x_true,Nc,τ)
+    z_true = sc.interp_linear(t_true,x_true,Nc,τ)
 
     if kk % 100 == 0:
         st_id = torch.randint(0, len(z_true) - batch_time, (1,)).item()
@@ -75,19 +66,15 @@ for kk in range(2000):
     id_sel = torch.randint(0, z_true.shape[0] - batch_time, (batch_size,))
     z_true_stack = torch.stack([z_true[id_sel + i, :] for i in range(batch_time)], dim=0)
     t_true_stack = torch.stack([t_true[id_sel + i] for i in range(batch_time)], dim=0)
-    # print(z_true_stack.shape)
-    # print(t_true_stack.shape)
 
     for i in range(0, batch_time):
-        fun = lambda t, x: model_torch(t, x, w0)
+        fun = lambda t, x: model(t, x, w0)
         if i == 0:
             z_pred = z_true_stack[i, :, :].reshape(1, z_true_stack.shape[1], z_true_stack.shape[2])
         else:
-            z_next = rk4(fun, t_true[i], z_pred[i - 1, :, :], dt)
+            z_next = sc.rk4(fun, t_true[i], z_pred[i - 1, :, :], dt)
             z_pred = torch.cat([z_pred, z_next.reshape(1, z_true_stack.shape[1], z_true_stack.shape[2])], 0)
 
-    # print("z_pred.shape=", z_pred.shape)
-    # print("z_true.shape=",z_true[0:batch_time,:].shape)
     # loss = torch.abs((z_true[0:batch_time,:]-z_pred)).sum() + torch.abs(w0).sum()
 
     # loss = (z_true_stack-z_pred).pow(2).mean() #+ 1e-5* torch.abs(w0).sum()
@@ -98,34 +85,41 @@ for kk in range(2000):
         w0_old = w0.detach().numpy()
         τ_old = τ.detach().numpy()
 
-        # print(τ_old)
         τ_arr.append(τ_old)
         τ_grad_arr.append(τ.grad.detach().numpy())
-        # print(w0.grad)
+
         w0 -= lr_w0 * w0.grad
         if kk > 300:
-            τ -= lr_tau * τ.grad * (10 * (kk % 10 == 0) + (kk % 10 != 0))
+            τ[1:] -= lr_tau * τ.grad[1:] * (10 * (kk % 10 == 0) + (kk % 10 != 0))
 
         loss_arr.append(loss.item())
-        # print(loss.item())
+
         if kk % 10 == 0:
-            axs = fig.add_subplot(1, 1, 1)
+            # Visualize
+            plt.cla()
+            print("Cleared figure.")
             for p_id in range(batch_size):
                 axs.plot(t_true_stack.detach().numpy()[:, p_id], z_pred[:, p_id, 0].detach().numpy(), 'ro')
                 axs.plot(t_true_stack.detach().numpy()[:, p_id], z_pred[:, p_id, 1].detach().numpy(), 'bo')
 
             axs.plot(t_true[0:len(z_true)], z_true[:, 0].detach().numpy(), 'r-')
             axs.plot(t_true[0:len(z_true)], z_true[:, 1].detach().numpy(), 'b-')
-
+            
             plt.show(block=False)
+
             plt.draw()
             plt.pause(0.001)
 
         if kk % 25 == 0:
             print("iter=", kk)
             print("loss=", loss.detach().numpy())
-            print("w0=", w0.detach().numpy())
+            #print("w0=", w0.detach().numpy())
             print("tau=", τ.detach().numpy())
 
-        w0.grad = None
+        
+        τ, w0 = sc.merge_vw(τ, w0, acc)
+        τ = torch.tensor(τ.detach().numpy(), device=device, requires_grad=True)
+        w0 = torch.tensor(w0.detach().numpy(), device=device, requires_grad=True)
+
         τ.grad = None
+        w0.grad = None
--- schemes.py
+++ schemes.py
@@ -9,10 +9,9 @@ def rk4(fun, t, x, h):
     return xn
 
 
-def interp_linear(t, x, nc, τ_arr):
-    if τ_arr.shape[0] > 1:
-        print("Only one tau is supported.")
-    for τ in τ_arr:
+def interp_linear(t, x, nc, τs):
+    flag = 0
+    for τ in τs:
         dt  = t[1]-t[0]
         τ_l = t[0:nc-1]
         τ_u = t[1:nc]
@@ -20,11 +19,68 @@ def interp_linear(t, x, nc, τ_arr):
         α = (τ >= τ_l) * (τ < τ_u)
         for i in range(len(x) - nc):
             A = torch.cat([x[i:i+nc-1].reshape(-1, 1), x[i+1:i+nc].reshape(-1, 1)], 1)
-            P = (α/dt).reshape(-1, 1) * torch.cat([(τ - τ_l).reshape(-1, 1), (τ_u - τ).reshape(-1, 1)], 1)
+            P = (α/dt).reshape(-1, 1) * torch.cat([(τ_u-τ).reshape(-1, 1), (τ - τ_l).reshape(-1, 1)], 1)
             z2 = (A * P).sum()
             if i == 0:
-                z_true = torch.cat([x[i].reshape(-1, 1), z2.reshape(-1, 1)], 1)
+                z_temp = z2.reshape(-1, 1)
             else:
-                z_temp = torch.cat([x[i].reshape(-1, 1), z2.reshape(-1, 1)], 1)
-                z_true = torch.cat([z_true, z_temp], 0)
-    return z_true
+                z_temp = torch.cat([z_temp, z2.reshape(-1, 1)], 0)
+        if flag==0:
+            z = z_temp
+            flag = 1
+        else:
+            z = torch.cat([z, z_temp], 1)
+    return z
+
+def merge(v,acc):
+    i = 0
+    L = len(v)
+    while i<L:
+        v0 = v[0:i] 
+        v1 = v[i+1:]
+        id0 = (v0-v[i]).abs()>acc
+        id1 = (v1-v[i]).abs()>acc
+
+        v0 = v0[id0]
+        v1 = v1[id1]
+
+        v  = torch.cat((v0,v[i].reshape(1,), v1),0)
+        if L==len(v):
+            i += 1
+        L = len(v)
+    return v
+
+def merge_vw(v,W,acc):
+    i = 0
+    L = len(v)
+    while i<L:
+        v0 = v[0:i] 
+        v1 = v[i+1:]
+        id0 = (v0-v[i]).abs()>acc
+        id1 = (v1-v[i]).abs()>acc
+        
+        # for v
+        v0 = v0[id0]
+        v1 = v1[id1]
+        v  = torch.cat((v0,v[i].reshape(1,), v1),0)
+        
+        # for W
+        Wr0 = W[0:i,:]
+        Wr1 = W[i+1:,:]
+
+        Wr0 = Wr0[id0,:]
+        Wr1 = Wr1[id1,:]
+        W   = torch.cat((Wr0,W[i,:].reshape(1,-1), Wr1),0)
+
+        Wc0 = W[:,0:i]
+        Wc1 = W[:,i+1:]
+
+        Wc0 = Wc0[:,id0]
+        Wc1 = Wc1[:,id1]
+
+        W   = torch.cat((Wc0,W[:,i].reshape(-1,1), Wc1),1)
+
+        if L==len(v):
+            i += 1
+        L = len(v)
+    return v, W
